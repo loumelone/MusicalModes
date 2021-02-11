de79757587f54b8a8465dcd6a49ed64c0e558276
loumelone <75758397+loumelone@users.noreply.github.com>
2021-02-11 14:49:04 -0500

    Initial Upload

---
+++ MusicalModes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MusicalModes
+//
+//  Created by Louis Melone on 2/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
