c7fab1c0aaece73e3a2c1b9d15e7489adbf42db2
loumelone <75758397+loumelone@users.noreply.github.com>
2021-02-24 13:34:38 -0500

    chords added

--- MusicalModes/ModesModel.swift
+++ MusicalModes/ModesModel.swift
@@ -11,12 +11,14 @@ import SwiftUI
 struct ModesModel {
     
     private(set) var modes: Array<Mode> = [(Mode(name: "Ionian", progressions: ["I-IV-V","I-vi-ii-V","ii-V-I","I-V-vi-IV"], chords: ["CMaj", "Dmin", "Emin", "FMaj", "GMaj", "Amin", "Bdim"] )),
-                                           (Mode(name: "Dorian", progressions: ["ii-IV"], chords: ["C", "D", "Eb/D#", "G", "A", "Bb/A#", "C" ] )),
-                                           (Mode(name: "Phyrigian", progressions: ["ii-IV"], chords: ["C", "Db/C#", "Eb/D#", "G", "Ab/G#", "Bb/A#", "B" ] )),
-                                           (Mode(name: "Lydian", progressions: ["ii-IV"], chords: ["C", "D", "E", "F", "G", "A", "B" ] )),
-                                           (Mode(name: "Mixolydian", progressions: ["ii-IV"], chords: ["C", "D", "E", "F", "G", "A", "B" ] )),
-                                           (Mode(name: "Aeolian", progressions: ["ii-IV"], chords: ["C", "D", "E", "F", "G", "A", "B" ] )),
-                                           (Mode(name: "Locrian", progressions: ["ii-IV"], chords: ["C", "D", "E", "F", "G", "A", "B" ] ))]
+                                           (Mode(name: "Dorian", progressions: ["ii-IV"], chords: ["Cmin", "Dmin", "EbMaj", "FMaj", "Gmin", "Adim", "BbMaj" ] )),
+                                           (Mode(name: "Phyrigian", progressions: ["ii-IV"], chords: ["Cmin", "DbMaj", "EbMaj", "Fmin", "Gdim", "AbMaj", "Bbmin"])),
+                                           (Mode(name: "Lydian", progressions: ["ii-IV"], chords: ["CMaj", "DMaj", "Emin", "F#dim", "GMaj", "Amin", "Bmin" ] )),
+                                           (Mode(name: "Mixolydian", progressions: ["ii-IV"], chords: ["CMaj", "Dmin", "Edim", "FMaj", "Gmin", "Amin", "BbMaj" ] )),
+                                           (Mode(name: "Aeolian", progressions: ["ii-IV"], chords: ["Cmin", "Ddim", "EbMaj", "Fmin", "Gmin", "AbMaj", "BbMaj" ] )),
+                                           (Mode(name: "Locrian", progressions: ["ii-IV"], chords: ["Cdim", "DbMaj", "Ebmin", "Fmin", "GbMaj", "AbMaj", "Bbmin" ] ))]
+    
+    private(set) var ioanianSongs: Array<Song>
                                            
 }
 
@@ -26,14 +28,16 @@ struct Mode: Identifiable {
     var songs: Array<Song> = []
     var progressions: Array<String> = []
     var chords: Array<String> = []
+    var intervals: Array<String> = []
     var id: String
    
-    init(name: String, songs: Array<Song> = [], progressions: Array<String> = [], chords: Array<String> = []) {
+    init(name: String, songs: Array<Song> = [], progressions: Array<String> = [], chords: Array<String> = [], intervals: Array<String> = []) {
         self.name = name
         self.id = name
         self.chords = chords
         self.progressions = progressions
         self.songs = songs
+        self.intervals = intervals
     }
 }
 
